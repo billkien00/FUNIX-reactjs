f8b616c00997605f5773c177daf9a3bf3e7868af
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-08-31 09:04:52 +0900

    commit lab3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import MenuComponent from './components/MenuComponent';
+import { Navbar, NavbarBrand  } from 'reactstrap';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap'
-function App() {
-  return (
-    <div className="App">
+import {DISHES} from './shared/dishes'
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       dish : DISHES
+    }
+  }
+  
+
+  render() {
+    return (
+      <div>
        <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">this is lab 2</NavbarBrand>
             
           </div>
       </Navbar>
+      <MenuComponent dishes={this.state.dish} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/MenuComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'
+
+
+
+export default class MenuComponent extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+           
+        }
+    }
+    
+    render() {
+        const menu = this.props.dishes.map((dish)=>{
+            return (
+                <div key={dish.id} className='col-12 col-md-5 m-1'>
+                    <Card>
+                        <CardImg width='100%' src={dish.image} alt={dish.name}></CardImg>
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            )
+        });
+        return (
+            <div className='container'>
+                <div className='row'>
+                    {menu}
+                </div>
+                
+            </div>
+        )
+    }
+}
