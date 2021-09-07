47b5330a6559fbf181efd3b6fd3b615985549300
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-09-07 11:52:07 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
-import MenuComponent from './components/MenuComponent';
-import { Navbar, NavbarBrand  } from 'reactstrap';
 import './App.css';
-import {DISHES} from './shared/dishes'
+import MainCoponent from './components/MainCoponent';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       dish : DISHES
-    }
-  }
+ 
   
 
   render() {
     return (
       <div>
-       <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">this is lab 2</NavbarBrand>
-            
-          </div>
-      </Navbar>
-      <MenuComponent dishes={this.state.dish} />
+       <MainCoponent/>
     </div>
     );
   }
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react'
-import { Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle } from 'reactstrap'
+import React from 'react'
+import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap'
 
-export default class DishdetailComponent extends Component {
-    constructor(props) {
-        super(props)
-    }
+export default function DishdetailComponent({ dish }) {
 
-    renderDish() {
-        const { data } = this.props
-        if (data !== null) {
+    const renderDish = ()=> {
+        if (dish) {
             return (
-                <div className='row'>
-                    <div className='col-12 col-md-5 m-1'>
-                        <Card>
-                            <CardImg width='100%' src={data.image} alt={data.name}></CardImg>
-                            <CardBody>
-                                <CardTitle>{data.name}</CardTitle>
-                                <CardText>{data.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className='col-12 col-md-5 m-1'>
-                        <h1>Comments</h1>
-                        {data.comments.map((comment) => {
-                            return (
-                                <div key={comment.id}>
-                                    <p>{comment.comment}</p>
-                                    <p>--{comment.author},{comment.date}</p>
-                                </div>
-                            )
-                        })}
+                <div className='container'>
+
+                    <div className='row'>
+                        <div className='col-12 col-md-5 m-1'>
+                            <Card>
+                                <CardImg width='100%' src={dish.image} alt={dish.name}></CardImg>
+                                <CardBody>
+                                    <CardTitle>{dish.name}</CardTitle>
+                                    <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className='col-12 col-md-5 m-1'>
+                            <h1>Comments</h1>
+                            {dish.comments.map((comment) => {
+                                return (
+                                    <div key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p>--{comment.author},{comment.date}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
 
@@ -38,12 +37,18 @@ export default class DishdetailComponent extends Component {
             return <div />
         }
     }
-    render() {
-        return (
 
-            <div>
-                {this.renderDish()}
-            </div>
-        )
-    }
+    return (
+        <div>
+            {renderDish()}
+        </div>
+    )
 }
+
+
+
+
+
+
+
+
