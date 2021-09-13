ef12176a6cd069ca6b152f74bbd8b868f37ad84c
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-09-14 02:05:46 +0900

    done

--- src/components/MoneyComponent.js
+++ src/components/MoneyComponent.js
@@ -1,28 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function MoneyComponent(props) {
-    const staff = props.staffs.map((staff)=>{
-        const salary = parseInt(staff.salaryScale)
-        const money = parseInt(staff.salaryScale)*3000000+parseInt(staff.overTime)*200000
-        return (
-            <div className='card rounded col-sm-12 col-md-5 col-lg-3 m-2'>
-                
-                <div class="card-body">
-                    <h4 class="card-title">{staff.name}</h4>
-                    <p class="card-text">Mã nhân viên: {staff.id} </p>
-                    <p class="card-text">Hệ số lương: {salary}</p>
-                    <p class="card-text">Số giờ làm thêm: {staff.overTime} </p>
-                    <p class="card-text bg-primary text-white p-2">Lương: {money} </p>
-                </div>
-                                
-            </div>
-        )
-    }) 
+  const staff = props.staffs.map((staff) => {
+    const salary = parseInt(staff.salaryScale);
+    const money =
+      parseInt(staff.salaryScale) * 3000000 + parseInt(staff.overTime) * 200000;
     return (
-        <div className='container-fluid'>
-            <div className='row'>
-                {staff}
-            </div>
+      <div className="card rounded col-sm-12 col-md-5 col-lg-3 m-2">
+        <div class="card-body">
+          <h4 className="card-title">{staff.name}</h4>
+          <p className="card-text">Mã nhân viên: {staff.id} </p>
+          <p className="card-text">Hệ số lương: {salary}</p>
+          <p className="card-text">Số giờ làm thêm: {staff.overTime} </p>
+          <p className="card-text bg-primary text-white p-2">Lương: {money} </p>
         </div>
-    )
+      </div>
+    );
+  });
+  return (
+    <div className="container-fluid">
+      <div className="row">{staff}</div>
+    </div>
+  );
 }
