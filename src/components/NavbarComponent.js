d2cc0c6b82ddc0cbb360e24a17aeb615c122f97b
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-09-17 22:58:32 +0900

    done

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,81 +1,137 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+// import React from "react";
+// import PropTypes from "prop-types";
+// import AppBar from "@mui/material/AppBar";
+// import Tabs from "@mui/material/Tabs";
+// import Tab from "@mui/material/Tab";
+// import Box from "@mui/material/Box";
+// import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+// import { makeStyles } from "@mui/styles";
+// import Link from "@mui/material/Link";
+
+// function TabPanel(props) {
+//   const { children, value, index, ...other } = props;
+
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && (
+//         <Box p={3}>
+//           <Typography>{children}</Typography>
+//         </Box>
+//       )}
+//     </div>
+//   );
+// }
+
+// TabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.any.isRequired,
+//   value: PropTypes.any.isRequired,
+// };
+
+// function a11yProps(index) {
+//   return {
+//     id: `simple-tab-${index}`,
+//     "aria-controls": `simple-tabpanel-${index}`,
+//   };
+// }
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//     backgroundColor: theme.palette.background.paper,
+//   },
+// }));
+
+// export default function SimpleTabs() {
+//   //const classes = useStyles();
+//   const [value, setValue] = React.useState(0);
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   };
+
+//   return (
+//     <div>
+//       <AppBar position="static">
+//         <Toolbar>
+//           <Tabs
+//             value={value}
+//             onChange={handleChange}
+//             aria-label="simple tabs example"
+//           >
+//             <Link href="/staffs">
+//               <Tab label="Nhân Viên" {...a11yProps(0)} />
+//             </Link>
+//             <Link href="/department">
+//               <Tab label="Phòng Ban" {...a11yProps(1)} />
+//             </Link>
+//             <Link href="/money">
+//               <Tab label="Bảng Lương" {...a11yProps(2)} />
+//             </Link>
+//           </Tabs>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
+
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+const NavbarComponent = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">
+          <i class="bi bi-house"></i>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Link className="m-2" to="/staffs/">
+                Nhân Viên
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="m-2" to="/department">
+                Phòng Ban
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="m-2" to="/money">
+                Bảng Lương
+              </Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
 };
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
-export default function SimpleTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="simple tabs example"
-        >
-          <Link to="/staffs">
-            <Tab label="Nhân Viên" {...a11yProps(0)} />
-          </Link>
-          <Link to="/department">
-            <Tab label="Phòng Ban" {...a11yProps(1)} />
-          </Link>
-          <Link to="/money">
-            <Tab label="Bảng Lương" {...a11yProps(2)} />
-          </Link>
-        </Tabs>
-      </AppBar>
-   
-    </div>
-  );
-}
+export default NavbarComponent;
