db7e874a2600ac45caf3865154e0ff0717e5cafc
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-09-20 00:58:08 +0900

    notthing

--- src/redux/staffs.js
+++ src/redux/staffs.js
@@ -11,14 +11,14 @@ export const Staffs = (
 ) => {
   switch (action.type) {
     case ActionTypes.ADD_STAFF:
-        return {...state, staffs = action.payload}
+        return {...state, isLoading = false, errMessage: null, staffs = action.payload}
       break;
     case ActionTypes.STAFF_FAILED:
-        return {...state, }
+        return {...state, isLoading = false, errMessage: action.payoad, staffs: [] }
 
       break;
     case ActionTypes.STAFF_LOADING:
-        return {...state, isLoading = {err} }
+        return {...state, isLoading = true, errMessage: null, staffs: [] }
       break;
 
     default:
