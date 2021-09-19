c457ee44deddc93a44ed86c70204737471d6f6c7
billkien00 <89521288+billkien00@users.noreply.github.com>
2021-09-19 14:26:27 +0900

    done fetch from sever

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -2,7 +2,7 @@ import * as ActionTypes from "./ActionTypes";
 import { baseUrl } from "../shared/baseUrl";
 
 export const addComment = (dishId, rating, author, comment) => ({
-  type: ActionType.ADD_COMMENT,
+  type: ActionTypes.ADD_COMMENT,
   payload: {
     dishId: dishId,
     rating: rating,
